34853a0a55fb296e0a1b15fece66014f2e1dd0e6
Mlungisi Sibitane <sibitanemlungisi26@gmail.com>
2025-10-26 15:26:42 +0200

    Add emailjs service and template code id

--- handlers/email.js
+++ handlers/email.js
@@ -3,12 +3,12 @@
   emailjs.init("zZuWx1PcPMwGuBQYn");
 })();
 
-// Optional: You can also add functions here to send emails, e.g.
+// function to send email
 function sendEmail(templateParams) {
   return emailjs.send("service_7ouy9fv", "template_chz5zq8", templateParams)
     .then((response) => {
-      console.log("SUCCESS!", response.status, response.text);
+      console.log("Success!", response.status, response.text);
     }, (error) => {
-      console.error("FAILED...", error);
+      console.error("Failed...", error);
     });
 }
