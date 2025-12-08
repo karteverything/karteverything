3aeff2c16d77f68cb029fa09fdfea2583172edfc
KArt Everything <sibitanemlungisi26@gmail.com>
2025-12-08 20:05:23 +0200

    Update analytics.js

--- handlers/analytics.js
+++ handlers/analytics.js
@@ -1,6 +1,9 @@
-// setup google analytic
+// setup google analytics
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+
+function gtag(){
+  dataLayer.push(arguments);
+}
 gtag('js', new Date());
 
 gtag('config', 'G-NMCJXXWRWB');
